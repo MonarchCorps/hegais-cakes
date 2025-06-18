a6bff8d7188b9473fdaf09a6b6e807eef0979702
MonarchCorps <monarchcorps01@gmail.com>
2025-06-19 00:29:19 +0100

    shop page responsive

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -48,20 +48,20 @@ export default async function ShopPage() {
                 </div>
             </section>
 
-            <div className="py-12 px-6 lg:px-24 flex flex-col gap-8">
-                <h1 className="text-2xl font-semibold text-[#0F4C81]">All Cakes</h1>
+            <div className="py-12 px-5 flex flex-col gap-8 max-w-[80rem] mx-auto">
+                <h1 className={`${berkshireSwash.className} font-semibold text-[#0F4C81] text-4xl`}>All Cakes</h1>
 
-                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
+                <div className="grid grid-cols-[repeat(auto-fill,minmax(20rem,1fr))] gap-10">
                     {products.map((product: Product) => (
                         <Link href={`/shop/${product.id}`} key={product.id} className="flex flex-col gap-2">
                             <img
                                 src={product.image1}
                                 alt={product.name}
-                                className="h-[400px] w-full object-cover rounded-md shadow-md"
+                                className="h-[400px] w-full object-cover rounded-md"
                             />
-                            <span className="text-lg font-semibold">{product.name}</span>
-                            <span className="text-sm text-gray-700 line-clamp-2">{stripHtml(product.description)}</span>
-                            <span className="text-base text-[#0F4C81] font-bold">₦{product.price}</span>
+                            <span className="text-2xl font-semibold">{product.name}</span>
+                            <span className="text-xl text-[#50555C] line-clamp-2">{stripHtml(product.description)}</span>
+                            <span className="text-2xl text-[#0F4C81] font-bold">£{product.price}</span>
                         </Link>
                     ))}
                 </div>
@@ -79,7 +79,7 @@ export default async function ShopPage() {
                                 className="absolute inset-0 size-full object-cover object-top-right -z-10"
                             />
                             <div className="absolute inset-0 flex flex-col items-center justify-center">
-                                <h1 className={`${berkshireSwash.className} text-center text-5xl leading-tight capitalize text-white`}>
+                                <h1 className={`${berkshireSwash.className} text-center text-5xl leading-tight capitalize text-white max-[510px]:text-3xl mb-3`}>
                                     Need something custom? Book a Consultation!
                                 </h1>
                                 <p className="mb-6 text-white text-center text-lg max-w-[44rem] mx-auto">
