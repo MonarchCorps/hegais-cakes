0dc80266813618d970b98b3721670fbfbc051e2d
MonarchCorps <monarchcorps01@gmail.com>
2025-06-19 01:36:19 +0100

    added ocra domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'orca-app-h75k3.ondigitalocean.app',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
